d8a5d3f6b5250cd3cda3a1dc9bc07beb8ead602d
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-03-24 19:49:44 -0300

    add arquivo alterar_background.js

---
+++ assets/js/alterar_background.js
@@ -0,0 +1,25 @@
+//Pomodoro - vermelho
+let fundoVermelho = document.querySelector('#pomodoro')
+
+fundoVermelho.addEventListener('click', function () {
+    fundoVermelho.style.backgroundColor = "rgb(184, 20, 20)"
+})
+
+
+//Intervalo Curto - verde
+let fundoVerde = document.querySelector('#intervaloCurto')
+
+fundoVerde.addEventListener('click', function () {
+    fundoVerde.style.backgroundColor = "rgb(21, 160, 95)"
+})
+
+
+//Intervalo Longo - azul
+let fundoAzul = document.querySelector('#intervaloLongo')
+
+fundoAzul.addEventListener('click', function () {
+    fundoAzul.style.backgroundColor = "rgb(69, 67, 114)"
+})
+
+
+//////////////// NÃO ESTÃO FUNCIONANDO ////////////////
