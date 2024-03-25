82f0b5448f33d2e8bb8363d1bc8788e853e57f76
cbiveu <cbiveu@gmail.com>
2024-03-25 17:42:29 -0300

    parte do modal relacionado a localStorage  funcionando

--- assets/js/vs01.js
+++ assets/js/vs01.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   const username = localStorage.getItem('username');
   const pomodoroDuration = localStorage.getItem('pomodoroDuration');
+  const pomodoroPause = localStorage.getItem('pomodoroPause');
+  const pomodoroLongPause = localStorage.getItem('pomodoroLongPause');
   const difficulty = localStorage.getItem('difficulty');
 
   // Se algum valor estiver ausente, exibe o modal
@@ -10,11 +12,14 @@ document.addEventListener('DOMContentLoaded', function () {
     // Se os valores estiverem presentes, preenche os campos
     document.getElementById('username').value = username;
     document.getElementById('pomodoroDuration').value = pomodoroDuration;
+    document.getElementById('pomodoroPause').value = pomodoroPause;
+    document.getElementById('pomodoroLongPause').value = pomodoroLongPause;
     document.getElementById('difficulty').value = difficulty;
   }
 });
 
+// exibe o modal de configurações
 function modalPomodoroConfig() {
-  const hiddenDiv = document.getElementById('conteudoOculto');
-  hiddenDiv.classList.toggle('hidden-config');
+  const hiddenDiv = document.getElementById('modal');
+  hiddenDiv.classList.toggle('showModal');
 }
