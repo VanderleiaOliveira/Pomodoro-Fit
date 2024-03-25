d0b8c08a3e996dfbb69358a0630f6e639fefee0f
cbiveu <cbiveu@gmail.com>
2024-03-25 20:32:13 -0300

    começado a inserir os elementos desenvolvidos para UI

---
+++ assets/js/alterar_background.js
@@ -0,0 +1,29 @@
+let body = document.querySelector('#body')
+let button = document.getElementsByTagName('button')
+
+//Beginner - verde
+Beginner.addEventListener('click', function () {
+    body.classList.toggle('fundoVerde')
+
+    for (let b = 0; b < button.length; b++) {
+        button[b].classList.toggle('fundoVerde')
+    }
+})
+
+// //Intermediate - azul
+Intermediate.addEventListener('click', function () {
+    body.classList.toggle('fundoAzul')
+
+    for (let b = 0; b < button.length; b++) {
+        button[b].classList.toggle('fundoAzul')
+    }
+})
+
+// // //Advanced - vermelho
+Advanced.addEventListener('click', function () {
+    body.classList.toggle('fundoVermelho')
+
+    for (let b = 0; b < button.length; b++) {
+        button[b].classList.toggle('fundoVermelho')
+    }
+})
