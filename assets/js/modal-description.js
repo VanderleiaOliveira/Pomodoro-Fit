faca584be9ba81ae4ade2be3d55e06160413d0c6
Jaqueline Marina <jaquelinemarina@hotmail.com>
2024-03-28 13:53:07 -0300

    add modal-description.js

---
+++ assets/js/modal-description.js
@@ -0,0 +1,19 @@
+function showModal() {
+    const modalBg = document.getElementById('modal');
+    modalBg.classList.add('showModal');
+  }
+  
+  function hideModal() {
+    const modalBg = document.getElementById('modal');
+    modalBg.classList.remove('showModal');
+  }
+  
+  function showDescriptionModal() {
+    const modalDescription = document.getElementById('modal-description');
+    modalDescription.classList.add('showModal');
+  }
+  
+  function hideDescriptionModal() {
+     const modalDescription = document.getElementById('modal-description');
+    modalDescription.classList.remove('showModal');
+  }
