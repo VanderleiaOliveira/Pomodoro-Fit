6fadbc2c2025282010b9bcff7a44c8925d81103f
cbiveu <cbiveu@gmail.com>
2024-03-28 04:17:22 -0300

    faltou mencionar no commit anterior que foram colocados sons no fim do pomodoro e no fim da pausa

--- assets/js/vs01.js
+++ assets/js/vs01.js
@@ -394,11 +394,11 @@ function startStretchingPause() {
     ? parseInt(localStorage.getItem('pomodoroLongPause'))
     : parseInt(localStorage.getItem('pomodoroPause'));
 
-  console.log(
-    `Iniciando ${
-      isLongPause ? 'uma pausa longa' : 'uma pausa curta'
-    } de ${pauseDuration} minutos.`
-  );
+  // console.log(
+  //   `Iniciando ${
+  //     isLongPause ? 'uma pausa longa' : 'uma pausa curta'
+  //   } de ${pauseDuration} minutos.`
+  // );
 
   pauseTimeLeftInSeconds = pauseDuration * 60;
   updatePauseCountDownDisplay();
